51dd7756cbc4790c62cf4350d6816a920aa556c1
johnliu-code <79877649+johnliu-code@users.noreply.github.com>
2023-04-04 11:23:52 -0400

    1. Dashboard and user login router 2. Navigation menu and switch page 3. Display data on the page from database

--- Booking Test/View/MainView.xaml.cs
+++ Booking Test/View/MainView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace Booking_Test.View
 {
@@ -22,6 +24,40 @@ namespace Booking_Test.View
         public MainView()
         {
             InitializeComponent();
+            this.MaxHeight=SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, (IntPtr)2, (IntPtr)0);
+        }
+
+        private void pnlControlBar_MouserEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState=WindowState.Maximized;
+            }
+            else this.WindowState=WindowState.Normal;
         }
     }
 }
--- Booking Test/ViewModel/MainViewModel.cs
+++ Booking Test/ViewModel/MainViewModel.cs
@@ -5,17 +5,24 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 using Booking_Test.Model;
 using Booking_Test.Repositories;
+using FontAwesome.Sharp;
 
 namespace Booking_Test.ViewModel
 {
-    public class MainViewModel: ViewModelBase
+    public class MainViewModel : ViewModelBase
     {
         // Fields
         private UserAccountModel _currentUserAccount;
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
         private IUserRepository userRepository;
 
+        // Properties
         public UserAccountModel CurrentUserAccount
         {
             get => _currentUserAccount;
@@ -26,11 +33,65 @@ namespace Booking_Test.ViewModel
             }
         }
 
+        public ViewModelBase CurrentChildView
+        {
+            get => _currentChildView;
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+        public string Caption
+        {
+            get => _caption;
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+        public IconChar Icon
+        {
+            get => _icon;
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        //--> Commands
+        public ICommand ShowHomeViewCommand {get;}
+        public ICommand ShowCustomerViewCommand { get; }
+
         public MainViewModel()
         {
             userRepository = new UserRepository();
             CurrentUserAccount = new UserAccountModel();
+
+            //Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowCustomerViewCommand = new ViewModelCommand(ExecuteShowCustomerViewCommand);
+
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+
             LoadCurrentUserData();
+        }
+
+        private void ExecuteShowCustomerViewCommand(object obj)
+        {
+            CurrentChildView = new CustomerViewModel();
+            Caption = "Customer";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
         }
 
         private void LoadCurrentUserData()
@@ -40,7 +101,7 @@ namespace Booking_Test.ViewModel
             {
 
                 CurrentUserAccount.UserName = user.Username;
-                CurrentUserAccount.Displayname = $"Welcome {user.Username}, {user.Roll} :) ";
+                CurrentUserAccount.Displayname = $"{user.Username}, {user.Roll}";
                 CurrentUserAccount.ProfilePicture = null;
             }
             else
